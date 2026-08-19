0e0d564d115181ee1d6f3e7827a40cd8bf091177
abdelrahman-milhem <a.milhem.dev@gmail.com>
2026-08-19 14:33:21 +0300

    search

--- src/main/kotlin/com/waraqa/backend/service/BookService.kt
+++ src/main/kotlin/com/waraqa/backend/service/BookService.kt
@@ -1,4 +1,27 @@
 package com.waraqa.backend.service
 
-class BookService{
+import com.waraqa.backend.dto.BookResponseDto
+import com.waraqa.backend.dto.toDto
+import com.waraqa.repository.BookRepository
+import com.waraqa.repository.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class BookService(
+    private val bookRepository: BookRepository,
+    private val userRepository: UserRepository
+) {
+
+    fun getAllBooks(): List<BookResponseDto> {
+        return bookRepository.findAll().map { it.toDto() }
+    }
+
+    fun getMyBooks(userEmail: String): List<BookResponseDto> {
+        val user = userRepository.findByEmail(userEmail)
+            .orElseThrow { RuntimeException("User not found") }
+
+        val userId = user.userId ?: throw RuntimeException("User ID is missing")
+
+        return bookRepository.findAllByUserId(userId).map { it.toDto() }
+    }
 }
